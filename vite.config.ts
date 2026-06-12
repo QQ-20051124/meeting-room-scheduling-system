74c494566a992ec6ec0d8ecd3f630ddf4470af22
Vicky <3965808766@qq.com>
2026-06-12 16:58:00 +0800

    修改233

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [uni()],
@@ -9,5 +10,20 @@ export default defineConfig({
         additionalData: `@import "@/styles/variables.scss";`
       }
     }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    chunkSizeWarningLimit: 1000
   }
 })
